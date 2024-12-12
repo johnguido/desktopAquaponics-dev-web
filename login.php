dd2d868456e1bd383938deb86e598e43d92bf272
johnguido <connorwaggoner98@gmail.com>
2024-12-11 20:12:19 -0500

    PDO test

--- login.php
+++ login.php
@@ -4,13 +4,20 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 //need to class this out 
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "Hoshyblob23!";
-$db_name = "desktop_aquaponics";
-$conn = "";
+$host = 'localhost';
+$db   = 'desktop_aquaponics';
+$user = 'root';
+$pass = 'Hoshyblob23!';
+$charset = 'utf8mb4';
 
-$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);;
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
 
 $phone_number = $_POST['phone_number'];
 $password = $_POST['password'];
