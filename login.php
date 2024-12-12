6afc48af62d1b3611b04fe95b4d052acf0d20a2b
johnguido <connorwaggoner98@gmail.com>
2024-12-11 20:18:44 -0500

    mysql_connection test

--- login.php
+++ login.php
@@ -8,23 +8,14 @@ $host = 'localhost';
 $db   = 'desktop_aquaponics';
 $user = 'root';
 $pass = 'Hoshyblob23!';
-$charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+try{
+    $conn = mysqli_connection($host, $user, $pass, $db);
+}catch(mysqli_sql_exception){
+    echo "Could not connect to database...";
+}
 
-$pdo = new PDO($dsn, $user, $pass, $options);
-
-$phone_number = $_POST['phone_number'];
-$password = $_POST['password'];
-
-$stmt = $pdo->prepare('SELECT * FROM users WHERE 'phone_number' = ?;');
-$stmt->execute([$phone_number]);
-$user = $stmt->fetch();
-
-echo $user;
+if ($conn){
+    echo "Connected to database!";
+}
 ?>
