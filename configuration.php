90f3e75f08325084d3a27a0452ece357c2905c12
johnguido <connorwaggoner98@gmail.com>
2024-12-12 11:47:03 -0500

    small change for local database connection

--- configuration.php
+++ configuration.php
@@ -1,8 +1,7 @@
 <?php
 
 //1 = server 0 = local
-//one day these branches might differ so good to have
-$config = 0; 
+$config = 0;
 
 if($config == 1) {
     define("SQL_SERVER_NAME", "127.0.0.1");
@@ -14,7 +13,7 @@ if($config == 1) {
 else{
     define("SQL_SERVER_NAME", "127.0.0.1");
     define("SQL_DB_USERID", "root");
-    define("SQL_DB_PASSWORD", "Hoshyblob23!");
+    define("SQL_DB_PASSWORD", "root");
     define("SQL_DB_NAME", "desktop_aquaponics");
     define("SQL_DB_PORT", "3306");
 }
